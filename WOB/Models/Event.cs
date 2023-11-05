89b1f71274850831f184b5e9eb4717458b4fa3c5
rikumaru <rikuto37458@icloud.com>
2023-11-05 16:55:25 +0900

    いったんコミット

---
+++ WOB/Models/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WOB.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(EventType))]
+        public int EventTypeId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public string Time { get; set; }
+
+        public string? Place { get; set; }
+
+        public string? Description { get; set; }
+
+        public Boolean Valid { get; set; }
+        
+        public EventType Type { get; set; }
+    }
+}
